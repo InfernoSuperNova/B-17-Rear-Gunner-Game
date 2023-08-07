716ee66c7b4ab2e52ab9bf300ba185d2bde225a8
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-07 21:55:36 +1200

    Stage test

---
+++ Assets/PropellerSpin.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    [Multiline]
+    public string propellerSpeedDescription = "(In degrees per second)";
+    public float propellerSpeed = 100.0f;
+    //store the axis it should spin on
+    public Vector3 spinAxis;
+    private float currentAngle;
+    private float nextAngle;
+    private double physicsDelta = 0.0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAngle = 0.0f;
+        nextAngle = 0.0f;
+    }
+    // Update on physics frames
+    private void FixedUpdate()
+    {
+        physicsDelta = 0;
+        //rotate the propeller
+        currentAngle = nextAngle;
+        nextAngle = nextAngle + propellerSpeed * Time.fixedDeltaTime;
+        print(nextAngle);
+    }
+    
+    // Update on display frames
+    void Update()
+    {
+        physicsDelta += Time.deltaTime;
+        Quaternion currentAngleTransform = Quaternion.Euler(spinAxis * currentAngle);
+        Quaternion nextAngleTransform = Quaternion.Euler(spinAxis * nextAngle);
+        Quaternion change = Quaternion.Lerp(currentAngleTransform, nextAngleTransform, Time.fixedDeltaTime * (float)physicsDelta);
+        //apply rotation change
+        transform.rotation = change;
+    }
+
+}
---
+++ B 17 Rear Gunner Game/Aircraft notes.md
@@ -0,0 +1,5 @@
+Should behave like someone would expect an aircraft to
+
+Perhaps store a target as an object reference?
+
+Store each component as a separate object, and have it pop off when destroyed
