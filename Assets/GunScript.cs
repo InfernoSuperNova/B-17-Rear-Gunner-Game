4cc4966a8431e50722657420bfcf8148c5bac995
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-15 00:18:01 +1200

    Improved RoF system

--- Assets/GunScript.cs
+++ Assets/GunScript.cs
@@ -5,35 +5,49 @@ using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
-    public int tracerInterval = 5;
+    public float rateOfFire = 600; //rounds per minute
+    public int tracerInterval = 3;
     public GameObject bulletPrefab;
     public bool shoot = false;
+    public int chanceOfSkipTracer = 10;
 
     private int currentBullet = 0;
+    private float previousBulletShootTime;
+    private float bulletInterval;
     // Start is called before the first frame update
     void Start()
     {
-     
+        previousBulletShootTime = Time.time;
+        bulletInterval = 60.0f / rateOfFire;
     }
     void FixedUpdate()
     {
-        //Fire 10 rounds/sec
-        if (shoot)
-        {
-            
-            GameObject newBullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-            //get the script from the bullet
-            BulletBehavior bulletBehavior = newBullet.GetComponent<BulletBehavior>();
-            if (currentBullet % tracerInterval == 0)
-            {
-                bulletBehavior.tracerEnabled = true;
-            }
-            currentBullet++;
-        }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        float currentTime = Time.time;
+        if (shoot && currentTime - previousBulletShootTime > bulletInterval)
+        {
+            Quaternion bulletAngleOffset = Quaternion.Euler(90, 0, 0);
+            GameObject newBullet = Instantiate(bulletPrefab, transform.position, transform.rotation * bulletAngleOffset);
+            //get the script from the bullet
+            BulletBehavior bulletBehavior = newBullet.GetComponent<BulletBehavior>();
+            bulletBehavior.bulletDir = transform.forward;
+            if (currentBullet % tracerInterval == 0)
+            {
+                bulletBehavior.tracerEnabled = true;
+            }
+            int skipBullet = Random.Range(0, 100);
+            if (skipBullet < chanceOfSkipTracer)
+            {
+            }
+            else
+            {
+                currentBullet++;
+            }    
+            
+            previousBulletShootTime = currentTime;
+        }
     }
 }
