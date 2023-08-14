706532646bd08741d146972e660dfd574a64794e
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-08-14 18:18:08 +1200

    Rudimentary timed shooting system

--- Assets/GunScript.cs
+++ Assets/GunScript.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class GunScript : MonoBehaviour
 {
+    public int tracerInterval = 5;
     public GameObject bulletPrefab;
+    public bool shoot = false;
+
+    private int currentBullet = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(bulletPrefab, transform.position, transform.rotation);
+     
     }
-
+    void FixedUpdate()
+    {
+        //Fire 10 rounds/sec
+        if (shoot)
+        {
+            
+            GameObject newBullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            //get the script from the bullet
+            BulletBehavior bulletBehavior = newBullet.GetComponent<BulletBehavior>();
+            if (currentBullet % tracerInterval == 0)
+            {
+                bulletBehavior.tracerEnabled = true;
+            }
+            currentBullet++;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
