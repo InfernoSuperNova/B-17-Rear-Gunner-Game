09d31294fbd6c8ce7de2a3abfe83028e7d43f39e
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-09-11 17:10:56 +1200

    Enemy marker system

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -9,8 +9,11 @@ public class GameManager : MonoBehaviour
     private float enemySpawnTimerCurrent;
     public GameObject player;
     public Enemy Enemy;
+    public Canvas mainUI;
+    public Camera mainCamera;
+    public GameObject enemyMarker;
     public List<Enemy> enemies;
-    // Start is called before the first frame update
+    private Dictionary<Enemy, GameObject> enemyMarkers;
     void Start()
     {
         enemies = new List<Enemy>();
@@ -37,8 +40,46 @@ public class GameManager : MonoBehaviour
             if (enemy.transform.childCount == 0)
             {
                 enemies.Remove(enemy);
+                GameObject enemyMarker = enemyMarkers[enemy];
+                enemyMarkers.Remove(enemy);
+                Destroy(enemyMarker);
                 Destroy(enemy.gameObject);
+                continue;
             }
+
+            float offsetPosY = enemy.transform.position.y + 1.5f;
+
+            // Final position of marker above GO in world space
+            Vector3 offsetPos = new Vector3(enemy.transform.position.x, offsetPosY, enemy.transform.position.z);
+
+            Vector2 canvasPos;
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(offsetPos);
+
+            
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(mainUI.GetComponent<RectTransform>(), screenPoint, null, out canvasPos);
+            
+            if (enemyMarkers.ContainsKey(enemy))
+            {
+                enemyMarkers[enemy].transform.localPosition = canvasPos;
+                Vector3 playerToEnemyDirNorm = (enemy.transform.position - player.transform.position).normalized;
+                Vector3 cameraDir = mainCamera.transform.forward;
+                float dot = Vector3.Dot(playerToEnemyDirNorm, cameraDir);
+                if (dot < 0)
+                {
+                    enemyMarkers[enemy].SetActive(false);
+                }
+                else
+                {
+                    enemyMarkers[enemy].SetActive(true);
+                }
+                continue;
+            }
+            GameObject uiObject = Instantiate(enemyMarker, canvasPos, Quaternion.identity);
+
+            uiObject.transform.SetParent(mainUI.transform, false);
+            enemyMarkers.Add(enemy, uiObject);
+        }
         }
 
     }
