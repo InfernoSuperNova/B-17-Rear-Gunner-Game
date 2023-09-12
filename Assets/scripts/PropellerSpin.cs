5500404b098f904487564d59973754a47f17c815
Gxaps <36036803+InfernoSuperNova@users.noreply.github.com>
2023-09-12 23:54:19 +1200

    Add offset for spinning in weird axis configurations

--- Assets/scripts/PropellerSpin.cs
+++ Assets/scripts/PropellerSpin.cs
@@ -8,8 +8,10 @@ public class PropellerSpin : MonoBehaviour
     [Multiline]
     public string propellerSpeedDescription = "(In degrees per second)";
     public float propellerSpeed = 100.0f;
+    
     //store the axis it should spin on
     public Vector3 spinAxis;
+    public Vector3 offset;
     private float currentAngle;
     private float nextAngle;
     private double physicsDelta = 0.0;
@@ -32,8 +34,8 @@ public class PropellerSpin : MonoBehaviour
     void Update()
     {
         physicsDelta += Time.deltaTime;
-        Quaternion currentAngleTransform = Quaternion.Euler(spinAxis * currentAngle);
-        Quaternion nextAngleTransform = Quaternion.Euler(spinAxis * nextAngle);
+        Quaternion currentAngleTransform = Quaternion.Euler(spinAxis * currentAngle + offset);
+        Quaternion nextAngleTransform = Quaternion.Euler(spinAxis * nextAngle + offset);
         Quaternion change = Quaternion.Lerp(currentAngleTransform, nextAngleTransform, Time.fixedDeltaTime * (float)physicsDelta);
         //apply rotation change
         transform.localRotation = change;
